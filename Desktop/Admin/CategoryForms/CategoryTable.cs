2cd5965ad4ce005cf67d7a7778cfcf8935b70424
Mirabbos2001 <mirabbosegamberdiyev7@gmail.com>
2023-11-14 21:14:15 +0500

    Category combaBox filter finished but search is not doing at the same time . Sardorbek  , Asilxo'ja and I did this tasks

--- Desktop/Admin/CategoryForms/CategoryTable.cs
+++ Desktop/Admin/CategoryForms/CategoryTable.cs
@@ -1,9 +1,11 @@
-﻿using Desktop.Extended;
+﻿using System.Windows.Forms;
+using Desktop.Extended;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using POS.Application.Common.DataTransferObjects.CategoryDtos;
 using POS.Application.Common.Models;
 using POS.Application.Interfaces;
+using POS.Domain.Entities;
 using static System.Net.Mime.MediaTypeNames;
 
 namespace Desktop.Admin.CategoryForms;
@@ -11,6 +13,13 @@ public partial class CategoryTable : UserControl
 {
     private readonly List<CategoryDto> _categories = new();
     private IBusinessUnit _businessUnit;
+    private int SelectInfoCategory = 0;
+    private List<string> infCt = new()
+    {  
+        "Barchasi",
+        "Activlar",
+        "Arxivlar"
+    };
     private int selectedId = 0;
 
     public CategoryTable(IBusinessUnit businessUnit)
@@ -28,13 +37,43 @@ public partial class CategoryTable : UserControl
     {
         _businessUnit = Configuration.GetServiceProvider()
                                      .GetRequiredService<IBusinessUnit>();
-        await Task.Run(FillCategories);
+        InfoCategory.DataSource = infCt.ToArray();
     }
 
     /// <summary>
     /// Fill table with categories
     /// </summary>
     /// <returns></returns>
+    private async Task FillCategoriesForActive()
+    {
+        var list = await _businessUnit.CategoryService.GetAllAsync();
+        if (IsHandleCreated)
+        {
+            table.BeginInvoke(() =>
+            {
+                table.DataSource = list.Where(i => i.IsDeleted == false).Select(i =>
+                new { Id = i.Id, Nomi = i.Name })
+                .ToList();
+                _categories.AddRange(list);
+            });
+        }
+    }
+
+    private async Task FillCategoriesForArchives()
+    {
+        var list = await _businessUnit.CategoryService.GetAllAsync();
+        if (IsHandleCreated)
+        {
+            table.BeginInvoke(() =>
+            {
+                table.DataSource = list.Where(i => i.IsDeleted == true).Select(i =>
+                new { Id = i.Id, Nomi = i.Name })
+                .ToList();
+                _categories.AddRange(list);
+            });
+        }
+    }
+
     private async Task FillCategories()
     {
         var list = await _businessUnit.CategoryService.GetAllAsync();
@@ -137,19 +176,7 @@ public partial class CategoryTable : UserControl
     {
         selectedId = int.Parse(table.SelectedRows[0].Cells[0].Value.ToString());
     }
-    //public async Task<List<CategoryDto>> SearchCategoryAsync(string txt)
-    //{
-    //    return await Task.Run(() =>
-    //    {
-    //        if (_categories != null)
-    //        {
-    //            var result = _categories.Where(i => i.Name.ToLower().Contains(txt.ToLower())).Select(i =>
-    //            new { Id = i.Id, Nomi = i.Name }).ToList();
-    //            return result;
-    //        }
-    //        return new List<CategoryDto>();
-    //    });
-    //}
+
 
     private void search_textbox_TextChanged(object sender, EventArgs e)
     {
@@ -161,6 +188,7 @@ public partial class CategoryTable : UserControl
                                               .Contains(search_textbox.Text.ToLower()))
                                               .Select(i => new { Id = i.Id, Nomi = i.Name })
                                               .ToList();
+
                 if (searchResult != null && searchResult.Any())
                 {
                     table.DataSource = searchResult;
@@ -174,20 +202,6 @@ public partial class CategoryTable : UserControl
             else
             {
                 table.DataSource = _categories ?? new List<CategoryDto>();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
             }
         }
         catch (Exception)
@@ -196,5 +210,58 @@ public partial class CategoryTable : UserControl
         }
     }
 
+    private async void ArchiveBtn_Click(object sender, EventArgs e)
+    {
+        if (selectedId != 0)
+        {
+            try
+            {
+                var result = new Modal("Rostdan ham arxivlamoqchimisiz?").ShowDialog();
+                if (result == DialogResult.OK)
+                {
+                    await _businessUnit.CategoryService.ActionAsync(selectedId, ActionType.Archive);
+                    await Task.Run(FillCategories);
+                    new Toastr().ShowSuccess("Muvoffaqqiyatli arxivelandi");
+                }
+                else
+                {
+                    // Handle cancel or other cases if needed
+                }
+            }
+            catch (Exception)
+            {
+                new Toastr().ShowError("Arxivelashda hatolik yuz berdi");
+            }
+        }
+        else
+        {
+            new Toastr().ShowError("Arxivelash uchun kategoriyalardan birini tanlang!");
+        }
+    }
+
+    private async void InfoCategory_SelectedIndexChangedAsync(object sender, EventArgs e)
+    {
+        var tmp = InfoCategory.Text.ToString();
+        if (tmp == "Activlar")
+        {
+            SelectInfoCategory = 0;
+            table.DataSource = FillCategoriesForActive();
+            //await Task.Run(FillCategoriesForActive);
+        }
+
+        else if (tmp == "Arxivlar")
+        {
+            SelectInfoCategory = 1;
+            table.DataSource = FillCategoriesForArchives();
+            //await Task.Run(FillCategoriesForArchives);
+        }
+
+        else
+        {
+            SelectInfoCategory = 2;
+            table.DataSource = FillCategoriesForArchives();
+            //await Task.Run(FillCategories);
+        }
+    }
 
 }
